3f65e2f4bb17fe49e6a4d32a92ca391ca002591b
Maximo <maximo@balestrini.com.ar>
2021-09-30 15:50:12 -0300

    Initial upload

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -l
+
+echo "docker entrypoint"
+
+#time=$(date)
+#BLENDER_FILE=$1
+
+
+#blender -b -o /tmp/ -E CYCLES -f 1 
+#echo "Saludamos a $1" > $GITHUB_WORKSPACE/saludo.txt
+
+#echo "::set-output name=time::$time"
