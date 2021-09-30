b457d8528dad320537658c579001494fe75883ce
Maximo <maximo@balestrini.com.ar>
2021-09-30 16:03:32 -0300

    testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ echo "docker entrypoint"
 #BLENDER_FILE=$1
 
 
-#blender -b -o /tmp/ -E CYCLES -f 1 
+blender -b $1 -o ./test.jpg -E CYCLES -f 1 
 #echo "Saludamos a $1" > $GITHUB_WORKSPACE/saludo.txt
 
 #echo "::set-output name=time::$time"
